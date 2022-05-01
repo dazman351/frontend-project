6f5b6fd175da3e0ddf036aaf49682fcf35df2420
Anton <97913279+dazman351@users.noreply.github.com>
2022-05-02 00:47:14 +0300

    update

--- style/javascript.js
+++ style/javascript.js
@@ -2,6 +2,7 @@
     
     let header = $('#header');
     let slider = $('#slider');
+    let upbutton = $('#upbutton');
     let sliderH = slider.innerHeight();
     let scrollPos = $(window).scrollTop();
 
@@ -16,6 +17,35 @@
         } else {
             header.removeClass('fixed');
         }
+    })
+
+
+    $(window).on("scroll load resize", function() {
+        scrollPos = $(this).scrollTop();
+
+        if(scrollPos > sliderH + 100) {
+            upbutton.addClass('upbutton');
+        } else {
+            upbutton.removeClass('upbutton');
+        }
+
+
+
+    })
+
+
+    $("[data-scroll]").on('click', function(event) {
+        event.preventDefault();
+
+        let elementID = $(this).data("scroll");
+        let elementOffset = $(elementID).offset().top - 10;
+
+
+        $('html, body').animate({
+            scrollTop: elementOffset
+        })
+
+
     })
 
     
