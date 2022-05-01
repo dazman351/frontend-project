871e71419d3e278a8afc7011a6acf807fcad708e
Anton <97913279+dazman351@users.noreply.github.com>
2022-05-01 23:05:43 +0300

    add files

---
+++ style/javascript.js
@@ -0,0 +1,27 @@
+﻿$(function() {
+    
+    let header = $('#header');
+    let slider = $('#slider');
+    let sliderH = slider.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+
+    $(window).on('scroll load resize', function() {
+        scrollPos = $(this).scrollTop();
+        
+
+
+        if(scrollPos > sliderH + 100) {
+            header.addClass('fixed');
+        } else {
+            header.removeClass('fixed');
+        }
+    })
+
+    
+
+    
+    
+
+
+});
